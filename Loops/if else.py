b64347ba84f8caa0b37ac7ced13f18247356b906
dhakshan <dhakchanamoorthy23@gmail.com>
2022-12-29 19:09:49 +0530

    initial commit

---
+++ Loops/if else.py
@@ -0,0 +1,8 @@
+print("Hi, This is script to find the number is odd or even")
+n = int(input("Enter the number: "))
+if n%2 == 0:
+    print(n,"is even number")
+else:
+    print(n,"is odd number")
+
+print("Bye....")
