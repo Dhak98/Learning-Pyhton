fa41c79380995a53d1fe01eac12dc216c4f00a76
dhakshan <dhakchanamoorthy23@gmail.com>
2022-12-29 19:43:37 +0530

    Elif prac prog insertion

---
+++ Loops/elseif.py
@@ -0,0 +1,13 @@
+# Take two int values from user and print greatest among them.
+
+print("Enter the two number to find which is greater")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+if num1 > num2:
+    print(num1," is greater among them")
+elif num2 > num1:
+    print(num2," is greater among them")
+else:
+    print("Are you kidding, both are equal numbers")
+
+print("Thank you ......")
