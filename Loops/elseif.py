7b5adc3bb0cc68107de4e6283146d9b3629a6f20
dhakshan <dhakchanamoorthy23@gmail.com>
2022-12-30 10:34:32 +0530

    Elif loop example programs

--- Loops/elseif.py
+++ Loops/elseif.py
@@ -102,3 +102,31 @@ else:
 """
 ##################################################################################################
 
+"""
+# Qus
+
+Ask user to enter age, sex ( M or F ), marital status ( Y or N ) and then using following rules print their place of service.
+if employee is female, then she will work only in urban areas.
+
+if employee is a male and age is in between 20 to 40 then he may work in anywhere
+
+if employee is male and age is in between 40 t0 60 then he will work in urban areas only.
+
+And any other input of age should print "ERROR".
+
+# Ans
+
+
+age = int(input("Enter the age: "))
+sex = input("Enter you Gender( M or F ): ")
+ms = input("Enter you Marital Status( Y or N ): ")
+if sex == 'f':
+    print("You are allowed to work on urban areas only...")
+elif sex == 'm' and age >= 20 and age <= 40:
+    print("You can work anywhere")
+elif sex == 'm' and age >= 40 and age <= 60:
+    print("You are allowed to work on urban areas only...")
+else:
+    print("ERROR")
+
+"""
