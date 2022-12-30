d5114228f25f0403f1835f962f2d7b2dbae3a3b0
dhakshan <dhakchanamoorthy23@gmail.com>
2022-12-30 15:32:31 +0530

    for loop example programs

---
+++ Loops/For.py
@@ -0,0 +1,38 @@
+"""
+# qus print 
+
+*
+**
+***
+****
+*****
+
+# Ans:-
+
+for i in range(6):
+    for j in range(i):
+        print("*", end="")
+    print("")
+
+"""
+############################################
+
+"""
+# Qus 
+
+Print
+
+*****
+****
+***
+**
+*
+
+# Ans
+
+for i in range(5,0,-1):
+    for j in range(i):
+        print("*", end="")
+    print("")
+
+"""
