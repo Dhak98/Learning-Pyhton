8d40603ca323bb1fc1c4a0160fa7e75f23148d88
dhakshan <dhakchanamoorthy23@gmail.com>
2022-12-31 13:43:07 +0530

    List example programs

--- List/List in Python.py
+++ List/List in Python.py
@@ -5,8 +5,7 @@ print(type(a))
 
 """
 """
-
-"""
+# Program for get input for list from user and print that list and print max and min number in the list
 
 lst=[]
 n=int(input("Enter elements for list: "))
@@ -18,3 +17,5 @@ print(lst)
 
 print(max(lst), "is a maximum number in the list")
 print(min(lst), "is a minimum number in the list")
+
+"""
