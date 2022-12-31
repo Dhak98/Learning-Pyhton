d85c7952cda3ea586b4bc128a13e28aa7a6627ad
dhakshan <dhakchanamoorthy23@gmail.com>
2022-12-31 13:41:24 +0530

    List example programs

---
+++ List/List in Python.py
@@ -0,0 +1,20 @@
+"""
+a=[5]
+print(a)
+print(type(a))
+
+"""
+"""
+
+"""
+
+lst=[]
+n=int(input("Enter elements for list: "))
+
+for i in range(0,n):
+    ele = int(input())
+    lst.append(ele)
+print(lst)
+
+print(max(lst), "is a maximum number in the list")
+print(min(lst), "is a minimum number in the list")
