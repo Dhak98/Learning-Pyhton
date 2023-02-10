92f62169aff7db49edad7dc855fc54643268fe2b
Dhak98 <dhakchanamoorthy23@gmail.com>
2023-02-10 09:28:37 +0530

    To Understand file concept

--- File/File_concept.py
+++ File/File_concept.py
@@ -5,8 +5,21 @@ fo.write("\n\n\nLooser only know the value of victory. Don't worry, do battle fo
 fo.close
 
 """
+"""
 #To write a multi lines in txt file
 
 fo = open("Multiline.txt","w")
 my_content = ["Fistline\n","Secondline\n","Thirdline\n"]
 fo.writelines(my_content)
+
+"""
+#concatenate new line using for loop
+
+fo = open("txtforloop.txt","w")
+
+my_content = ["The First Line\n","The Second Line\n","The Third Line\n"]
+
+for lines in my_content:
+    fo.write(lines+"\n")
+
+fo.close
