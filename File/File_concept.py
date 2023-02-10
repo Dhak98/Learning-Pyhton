3713818c3b43082848b90e1b7dfaf1b251deafdf
Dhak98 <dhakchanamoorthy23@gmail.com>
2023-02-10 09:15:09 +0530

    To Understand file concept

--- File/File_concept.py
+++ File/File_concept.py
@@ -1,3 +1,4 @@
 fo = open("test1.txt","w")
-fo.write("Sample Document it is used to understand the concept of File Handling")
+fo.write("Sample Document it is used to understand the concept of File Handling\n")
+fo.write("\n\n\nLooser only know the value of victory. Don't worry, do battle for victory")
 fo.close
