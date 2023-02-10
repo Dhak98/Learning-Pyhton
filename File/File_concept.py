d841bc1e7b43d088ff0a5fa7a014915da8494825
Dhak98 <dhakchanamoorthy23@gmail.com>
2023-02-10 09:22:16 +0530

    To Understand file concept

--- File/File_concept.py
+++ File/File_concept.py
@@ -1,4 +1,12 @@
+"""
 fo = open("test1.txt","w")
 fo.write("Sample Document it is used to understand the concept of File Handling\n")
 fo.write("\n\n\nLooser only know the value of victory. Don't worry, do battle for victory")
 fo.close
+
+"""
+#To write a multi lines in txt file
+
+fo = open("Multiline.txt","w")
+my_content = ["Fistline\n","Secondline\n","Thirdline\n"]
+fo.writelines(my_content)
