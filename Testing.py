ce8fe6df6a1c5d9ee6a319b6dbd973cbfe742a00
dhak98 <dhakchanamoorthy23@gmail.com>
2023-02-24 09:43:18 +0530

    Updated

--- Testing.py
+++ Testing.py
@@ -26,6 +26,9 @@ arr[pos1],arr[pos2] = arr[pos2], arr[pos1]
 
 print(arr)
 """
+"""
+# to find the min and largest element in list
+
 arr = []
 n = int(input("Enter how many elements you want in list:"))
 
@@ -36,10 +39,27 @@ for i in range(0,n):
     
 print(arr)
 
-# to find the min and largest element in list
 
 small = min(arr)
 lar = max(arr)
 
 print("Smallest Element in arr is ", small)
 print("Largest Element in arr is ", lar)
+
+"""
+# To find Fibonacci Series
+
+n = int(input("Enter the number to find the fibonacci series: "))
+
+n1, n2 = 0, 1
+sum = 0
+
+if n<=0:
+    print("Please enter the number greter than 0")
+else:
+    for i in range(0, n):
+        print(sum, end=" ")
+        n1 = n2
+        n2 = sum
+        sum = n1 + n2
+        
