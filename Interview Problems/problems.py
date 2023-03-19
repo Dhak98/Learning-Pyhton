ffcf7dd604bea590db717982b290b380fe6fe772
Dhakchanamoorthy <dhakchanamoorthy23@gmail.com>
2023-03-19 17:10:21 +0530

    Update problems.py

--- Interview Problems/problems.py
+++ Interview Problems/problems.py
@@ -17,3 +17,19 @@ arr[pos1],arr[pos2] = arr[pos2], arr[pos1]
 print(arr)
 
 """
+
+"""
+in_str = input("Enter the string to Count the vowels:")
+vowel = ['a', 'e', 'i', 'o', 'u']
+str = in_str.lower()
+countp = 0
+countn = 0
+for i in str:
+    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i =='u':
+        countp += 1
+    
+if countp == 0:
+    print("No Vowels found") 
+else:
+    print("Vowels found, No.",countp)
+"""
